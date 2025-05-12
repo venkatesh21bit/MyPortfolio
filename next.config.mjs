34e2255b13832a20e027d8690b812b0b7fdb38b3
venkatesh21bit <venkatesh.k21062005@gmail.com>
2025-05-13 00:48:53 +0530

    eslint issue

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/about",
+        permanent: true, // Set to true for a 308 permanent redirect
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
